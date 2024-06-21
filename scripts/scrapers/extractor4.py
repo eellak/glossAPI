e8db2346bb2db3ad1d00386a2a89c0ae31e64a61
fffoivos <foivos.karounos@gmail.com>
2024-06-21 10:03:25 +0300

    fixed line 43 index error

--- scripts/scrapers/extractor4.py
+++ scripts/scrapers/extractor4.py
@@ -2,10 +2,10 @@ import argparse
 import os
 import json
 import logging
-import time
 from pdfminer.high_level import extract_text
 from pdfminer.pdfparser import PDFSyntaxError
 from pdfminer.psparser import PSEOF
+import time
 
 def setup_logging():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -24,29 +24,23 @@ def get_indexes(papers):
     if papers:
         nums = []
         for p in papers:
-            num = p.replace("paper_", "")
+            num = p.split("_")[-1]
             nums.append(int(num))
         return sorted(nums)[-1:]
     return []
 
-def process_pdfs(downloaded_files_path, little_potato_path):
-    extracted_files_path = os.path.join(downloaded_files_path, "extracted_pdfs")
-    problematic_file_path = os.path.join(extracted_files_path, "problematic_pdfs.json")
-    metadata_dict_path = os.path.join(extracted_files_path, "metadata_dict.json")
+def process_pdfs(downloaded_files_path, progress_report_path):
+    extracted_files_dir = os.path.join(downloaded_files_path, "extracted_pdfs")
+    problematic_file_path = os.path.join(extracted_files_dir, "problematic_pdfs.json")
+    extracted_files_path = os.path.join(extracted_files_dir, "extracted_files.json")
 
-    os.makedirs(extracted_files_path, exist_ok=True)
+    os.makedirs(extracted_files_dir, exist_ok=True)
     problematic_files = []
-    metadata_dict = read_json_file(metadata_dict_path)
-    little_potato = read_json_file(little_potato_path)
-    little_potato_values = []
-    little_potato_keys = []
+    extracted_files = read_json_file(extracted_files_path)
+    progress_report = read_json_file(progress_report_path)
     index = 1
-
-    if little_potato:
-        little_potato_values = list(little_potato.values())
-        little_potato_keys = list(little_potato.keys())
-        if little_potato_keys:
-            index = get_indexes(little_potato_keys)[0]
+    if extracted_files:
+            index = get_indexes(list(extracted_files.keys()))[0] + 1
 
     pdf_files = [f for f in os.listdir(downloaded_files_path) if f.endswith('.pdf')]
     if not pdf_files:
@@ -54,10 +48,10 @@ def process_pdfs(downloaded_files_path, little_potato_path):
         return
 
     logging.info("Starting the PDF processing script...")
+    processed = list(extracted_files.keys())
     for file_name in pdf_files:
         base_name = os.path.splitext(file_name)[0]
-
-        if metadata_dict.get(base_name, None) in little_potato_values or None:
+        if base_name in processed:
             logging.info(f"{file_name} has already been processed.")
             continue
 
@@ -65,13 +59,13 @@ def process_pdfs(downloaded_files_path, little_potato_path):
         try:
             text = extract_text(file_path)
             if text:
-                output_file_path = os.path.join(extracted_files_path, f"paper_{index}.txt")
-                index += 1
+                output_file_path = os.path.join(extracted_files_dir, f"paper_{index}.txt")
                 with open(output_file_path, 'w', encoding='utf-8') as f:
                     f.write(text)
                 logging.info(f"Extracted and saved text from {file_name}")
                 # Save metadata without the text file information
-                metadata_dict[base_name] = little_potato.get(file_name[:-4], "No metadata found")
+                extracted_files[f"paper_{index}"] = progress_report.get(file_name[:-4], "No metadata found")
+                index += 1
         except (PDFSyntaxError, PSEOF, Exception) as e:
             problematic_files.append({"file_name": file_name, "error": str(e)})
             logging.error(f"Error processing {file_name}: {type(e).__name__}: {e}")
@@ -81,14 +75,14 @@ def process_pdfs(downloaded_files_path, little_potato_path):
             json.dump(problematic_files, f, indent=4, ensure_ascii=False)
 
     # Save updated metadata dictionary
-    with open(metadata_dict_path, 'w', encoding='utf-8') as f:
-        json.dump(metadata_dict, f, indent=4, ensure_ascii=False)
+    with open(extracted_files_path, 'w', encoding='utf-8') as f:
+        json.dump(extracted_files, f, indent=4, ensure_ascii=False)
 
     logging.info("Files processing details have been updated and recorded.")
 
 if __name__ == "__main__":
-    start_time = time.time()  # Capture start time
     setup_logging()
+    start_time = time.time()
     parser = argparse.ArgumentParser(description="Process PDF files and associate extracted text with metadata.")
     parser.add_argument("--path", type=str, help="Path to the folder containing PDF files to be processed.")
     parser.add_argument("--json", type=str, help="Path to the JSON file containing metadata associations.")
