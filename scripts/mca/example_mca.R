d353bee69d255c72bdabd14aea94fa2c549d1d3b
fivos <foivos.karounos@gmail.com>
2024-09-20 12:22:37 +0300

    reorganized

--- scripts/mca/example_mca.R
+++
@@ -1,7 +0,0 @@
-library(ca)
-dt2 <- read.csv("annot_data_raw.dat", sep="\t", stringsAsFactors=F)
-dt3 <- na.omit(dt2)
-mjca1 <- mjca(dt2[dt2$Ποικιλία %in% c("κνε", "καθαρεύουσα") & !(dt2$Ύφος %in% c("τραγικό", "λυρικό", "κωμικό", "παραινετικό")),], )
-plot(mjca1)
-# coordinates
-mjca1$rowcoord
