ce41d88ac1bfede5d2adaff8aacd66657965c099
roulikcat <nvidras@gmail.com>
2024-09-30 00:40:53 +0300

    Standardised functions Preprocessor ready to input files into proper bpe encoder To Do add modularity to stripping

--- tokenization/preprocessing/preprocessor.c
+++ tokenization/preprocessing/preprocessor.c
@@ -26,85 +26,99 @@ int char_bytes(unsigned char input) {
 }
 
 void fullstrip(FILE* dst, FILE* src){
-    debug() ;
-    unsigned char character = 0 ;
-    fread(&character,sizeof(unsigned char),1,src) ;
-    printf("Character read is %c , with value %u \n", character, (unsigned char) character) ;
-    if( char_bytes(character) > utf_8_gr_eng ) { //too big to be greek or english
-        for( int i = 0 ; i < char_bytes(character)-1 ; i++ ) {
-            fread(&character,sizeof(unsigned char),1,src) ;
+    while( 1 ) {
+        if ( feof(src) ) {
+            break;
         }
-        return;
-    }
-    if ( char_bytes(character) == 1) {  //english
-        if( character == 10 || character == 13 || character == 20 || character == 32 ){ //spaces and newline
+        unsigned char character = 0 ;
+        fread(&character,sizeof(unsigned char),1,src) ;
+        if( char_bytes(character) > utf_8_gr_eng ) { //too big to be greek or english
+            for( int i = 0 ; i < char_bytes(character)-1 ; i++ ) {
+                fread(&character,sizeof(unsigned char),1,src) ;
+            }
+            continue ;
+        }
+        if ( char_bytes(character) == 1) {  //english
+            if( character == 10 || character == 13 || character == 20 || character == 32 ){ //spaces and newline
+                fwrite(&character,sizeof(unsigned char),1,dst) ;
+            }
+            else if ( character > 97 && character < 122  ){ // lower case letters
+                fwrite(&character,sizeof(unsigned char),1,dst) ;
+            }
+            else if ( character > 65 && character < 90 ){ //Upper case
+                fwrite(&character,sizeof(unsigned char),1,dst) ;
+            }
+        }
+        else {
+            unsigned char nextbyte = 0 ;
+            fread(&nextbyte,sizeof(unsigned char),1,src) ;
+            if( character > utf_8_gr_max_first_byte) { //too big to be greek
+                continue ;
+            }
+            if( character < utf_8_gr_min_first_byte ) { //too small to be greek
+                continue ;
+            }
+            if( character == 206 && (( nextbyte > utf_8_gr_206_max_second) || ( nextbyte < utf_8_gr_206_min_second) || ( nextbyte == 135 )) ) { //not a letter
+                continue ;
+            }
+            if( character == 207 && (( nextbyte > utf_8_gr_207_max_second) || ( nextbyte < utf_8_gr_207_min_second) )) { //not a letter
+                continue ;
+            }
             fwrite(&character,sizeof(unsigned char),1,dst) ;
+            fwrite(&nextbyte,sizeof(unsigned char),1,dst) ;
         }
-        else if ( character < 97 && character > 122  ){ // lower case letters
-            fwrite(&character,sizeof(unsigned char),1,dst) ;
-        }
-        else if ( character < 65 && character > 90 ){ //Upper case
-            fwrite(&character,sizeof(unsigned char),1,dst) ;
-        }
-    }
-    else {
-        unsigned char nextbyte = 0 ;
-        fread(&nextbyte,sizeof(unsigned char),1,src) ;
-        if( character > utf_8_gr_max_first_byte) { //too big to be greek
-            return ;
-        }
-        if( character < utf_8_gr_min_first_byte ) { //too small to be greek
-            return ;
-        }
-        if( character == 206 && (( nextbyte > utf_8_gr_206_max_second) || ( nextbyte < utf_8_gr_206_min_second) || ( nextbyte == 135 )) ) { //not a letter
-            return ;
-        }
-        if( character == 207 && (( nextbyte > utf_8_gr_207_max_second) || ( nextbyte < utf_8_gr_207_min_second) )) { //not a letter
-            return ;
-        }
-        fwrite(&character,sizeof(unsigned char),1,dst) ;
-        fwrite(&nextbyte,sizeof(unsigned char),1,dst) ;
     }
 }
 
-unsigned int create_code(FILE* src, unsigned char character) {
-    unsigned int temp ;
+unsigned short create_code(FILE* src, unsigned char character) {
+    unsigned short temp ;
     if ( char_bytes(character) == 1) {
         temp = character ;
     }
     else {
-        unsigned int nextbyte = fgetc(src) ;
+        unsigned char nextbyte = 0 ;
+        fread(&nextbyte,sizeof(unsigned char),1,src) ;
         temp = character ;
-        nextbyte = nextbyte << 8 ;
+        temp = temp << 8 ;
         temp = temp | nextbyte ;
     }
     return temp ;
 }
 
-// TO-DO expand for multiple layers
 void decode_text(FILE* dst, FILE* src) {
     while( 1 ) {
-        unsigned int temp ;
+        unsigned short temp = 0 ;
         if( feof(src) ) {
             return ;
         }
-        fread(&temp,sizeof(unsigned int), 1 , src) ;
-        if( temp < 128 ){
+        fread(&temp,sizeof(unsigned char), 1 , src) ;
+        if( temp == 0 ) {
+            continue ;
+        }
+        if( char_bytes(temp) == 1 ){
             unsigned char tchar = (char)temp ;
             fwrite(&tchar,sizeof(unsigned char),1,dst) ;
         }
         else {
-            unsigned short schar = (short)temp ;
+            unsigned short schar = create_code(src,temp) ;
             fwrite(&schar,sizeof(unsigned short),1,dst) ;
         }
     }
 }
 
-void encode_text(FILE* dst, FILE* src, unsigned char character) {
-    unsigned int output = create_code(src,character) ;
-    fwrite(&output,sizeof(unsigned int),1,dst) ;
-    if( output == 0xffff ) {
-        return ;
+void encode_text(FILE* dst, FILE* src) {
+    while( 1 ) {
+        if ( feof(src) ) {
+            break;
+        }
+        unsigned char character = 0 ;
+        fread(&character,sizeof(unsigned char),1,src) ;
+        printf("temp has value of %x \n",character) ;
+        unsigned short output = create_code(src,character) ;
+        fwrite(&output,sizeof(unsigned short),1,dst) ;
+        if( output == 0xffff ) {
+            return ;
+        }
     }
     return ;
 }
@@ -155,26 +169,18 @@ int main(int argc, char** argv) {
     if( !dstprovided ) {
         printf("Please provide destination file \n") ;
     }
-    unsigned char character ;
+    debug() ;
     if( mode == 1 ) {
-        while( 1 ) {
-            if ( feof(src) ) {
-                break;
-            }
-            character = fgetc(src) ;
-            encode_text(dst,src,character) ;
-        }
+        debug() ;
+        encode_text(dst,src) ;
     }
-    if( mode == 2 ) {
-        while( 1 ) {
-            if ( feof(src) ) {
-                break;
-            }
-            fullstrip(dst,src) ;
-        }
-    }
-    if ( mode == 3 ) {
+    if ( mode == 2 ) {
+        debug() ;
         decode_text(dst,src) ;
+    }
+    if( mode == 3 ) {
+        debug() ;
+        fullstrip(dst,src) ;
     }
     fclose(src) ;
     fclose(dst) ;
