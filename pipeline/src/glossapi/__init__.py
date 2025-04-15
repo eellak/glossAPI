27e1a4ef7eb4a4abbe5e28cd25ced6372321d221
fffoivos <foivos.karounos@gmail.com>
2025-04-15 15:31:22 +0300

    Remove unused academic_section.py file as part of pipeline refactoring

--- pipeline/src/glossapi/__init__.py
+++ pipeline/src/glossapi/__init__.py
@@ -12,7 +12,6 @@ This is an open source project that provides tools for linguistic annotations
 and text processing, with a special focus on the Greek language.
 """
 
-from .academic_section import AcademicSection
 from .gloss_extract import GlossExtract
 from .gloss_section_classifier import GlossSectionClassifier
 from .corpus import Corpus
@@ -21,7 +20,6 @@ from .gloss_section import Section, GlossSection
 from .gloss_downloader import GlossDownloader
 
 __all__ = [
-    'AcademicSection',
     'GlossExtract',
     'GlossSection',
     'GlossSectionClassifier',
